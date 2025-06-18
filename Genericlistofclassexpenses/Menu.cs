b096fe94430f11490f8ee18bf85ea00283909e11
Aparnnaa-a <aparnasreeja123@gmail.com>
2025-06-18 13:18:14 +0530

    June 18

---
+++ Genericlistofclassexpenses/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericlistofclassexpenses
+{
+    internal class Menu
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ Genericlistofclassexpenses/Program.cs
@@ -0,0 +1,12 @@
+﻿using Genericlistofclassexpenses;
+
+    List<Menu> menus = new List<Menu>
+{
+    new Menu { Name = "Chiken Biriyani", Description =  "Tasty"},
+    new Menu { Name = "Meals", Description = "Good" }
+};
+
+foreach (var menu in menus)
+{
+    Console.WriteLine($"{menu.Name} - {menu.Description}");
+}
